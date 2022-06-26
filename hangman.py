9a052511bac0d14bd8fa872356b5119430130944
Kshitij Arora <kshitij@Kshitijs-MacBook-Air.local>
2022-06-26 22:03:05 +0530

    added winning

--- hangman.py
+++ hangman.py
@@ -4,14 +4,24 @@ wordList = ["aardvark", "baboon", "camel"]
 #Choosing a random word from list
 chosenWord = random.choice(wordList)
 print(chosenWord)       #for testing
-guess = (input("Guess a letter?: ")).lower()    #user input
 
-#Checking if the guessed word is in the chosenWord
-for char in chosenWord:
-    if guess == char:
-        print("Yes")
-    else:
-        print("No")
+#Creating empty list
+guessWord = ['_'] * len(chosenWord)
+
+while ('_' in guessWord):
+
+    guess = (input("Guess a letter: ")).lower()    #user input
+
+    #Checking if the guessed word is in the chosenWord
+    i = 0
+    while i < len(chosenWord):
+        if chosenWord[i] == guess:
+            guessWord[i] = guess
+        
+        i += 1
+    print(guessWord)
+
+
 
 
 
