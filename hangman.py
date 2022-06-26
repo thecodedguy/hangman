c9156d240c7ce3ae4cc13eb6d654105cc569a45d
Kshitij Arora <kshitij@Kshitijs-MacBook-Air.local>
2022-06-26 22:29:36 +0530

    Added Losing and ASCII art

--- hangman.py
+++ hangman.py
@@ -1,5 +1,61 @@
 import random
 wordList = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 #Choosing a random word from list
 chosenWord = random.choice(wordList)
@@ -7,20 +63,31 @@ print(chosenWord)       #for testing
 
 #Creating empty list
 guessWord = ['_'] * len(chosenWord)
+lives = 6       #lives of user
 
-while ('_' in guessWord):
+while ('_' in guessWord and lives != 0):
 
     guess = (input("Guess a letter: ")).lower()    #user input
 
     #Checking if the guessed word is in the chosenWord
     i = 0
+    flag = False
     while i < len(chosenWord):
         if chosenWord[i] == guess:
             guessWord[i] = guess
-        
+            flag = True
         i += 1
-    print(guessWord)
+    if flag != True:
+        lives -= 1
+        print(f"\nWrong Choice!!\nLives left: {lives}\n" + stages[lives])
+    print(f"{' '.join(guessWord)}")
+    
 
+if lives == 0:
+    print("You lose")
+
+if '_' not in guessWord:
+    print("You Win")
 
 
 
